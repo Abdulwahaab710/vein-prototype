776d71467931daebd38c84f42912cf756a5ad5e8
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-09-15 17:18:17 -0400

    Added seeds to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+blood_types = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-'
+]
+
+blood_compatiblity = {
+  'A+': [
+    'A+',
+    'AB+'
+  ],
+  'A-': [
+    'A+',
+    'A-',
+    'AB+',
+    'AB-'
+  ],
+  'B+': [
+    'B+',
+    'AB+'
+  ],
+  'B-': [
+    'B+',
+    'B-',
+    'AB+',
+    'AB-'
+  ],
+  'AB+': [
+    'AB+'
+  ],
+  'AB-': [
+    'AB+',
+    'AB-'
+  ],
+  'O+': [
+    'A+',
+    'B+',
+    'AB+',
+    'O+'
+  ],
+  'O-': [
+    'A+',
+    'A-',
+    'B+',
+    'B-',
+    'AB+',
+    'AB-',
+    'O+',
+    'O-'
+  ]
+}
+
+blood_types.each do |blood_type|
+  BloodType.find_or_create_by(name: blood_type)
+end
+
+blood_compatiblity.keys.each do |blood_type|
+  donator = BloodType.find_by(name: blood_type.to_s)
+  blood_compatiblity[blood_type].each do |receiver|
+    puts receiver.to_s
+    receiver_blood_type = BloodType.find_by(name: receiver)
+    BloodCompatibility.find_or_create_by!(donator_id: donator.id, receiver_id: receiver_blood_type.id)
+  end
+end
